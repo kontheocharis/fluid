029f29dca60055c12365807bac0b320b8e8d038a
chrisbrown.guitar@googlemail.com <chrisbrown.guitar@googlemail.com>
2023-08-04 20:19:05 +0100

    Tweaks to show

--- src/Syntax2.hs
+++ src/Syntax2.hs
@@ -8,7 +8,7 @@ import Typecheck
 
 
 renderVal :: Value -> String 
-renderVal v = render (cPrint_ 0 0 (quote0 v))
+renderVal v = " (" ++ show (quote0 v) ++ " )"
 
 -- Inferrable Terms
 data TermInf2 = 
@@ -63,35 +63,35 @@ infToValue (LTEElimT _ _ _ _ _ _ v) = v
 
 
 renderInf :: TermInf2 -> String 
-renderInf (AnnT t1 t2 v) = "AnnT " ++ renderChk t1 ++ renderChk t2 ++ render (cPrint_ 0 0 (quote0 v))
-renderInf (StarT v) = "StarT " ++ renderVal v 
-renderInf (PiT t1 t2 v) = "PiT " ++ renderChk t1 ++ renderChk t2 ++ renderVal v
-renderInf (SigmaT t1 t2 v) = "SigmaT " ++ renderChk t1 ++ renderChk t2 ++ renderVal v
-renderInf (BoundT i v) = "BoundT " ++ show i ++ renderVal v 
-renderInf (FreeT n v) = "FreeT " ++ show n ++ renderVal v 
-renderInf (AppRedT ti tc v) = renderInf ti ++ " :@: " ++ renderChk tc ++ renderVal v 
-renderInf (AppT t1 t2 v) = "AppT " ++ renderChk t1 ++ renderChk t2 ++ renderVal v
-renderInf (PairT t1 t2 t3 t4 v) = "PairT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4 ++ renderVal v
-renderInf (NatT v) = "NatT " ++ renderVal v 
-renderInf (NatElimT t1 t2 t3 t4 v) = "NatElimT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4 ++ renderVal v
-renderInf (VecT t1 t2 v) = "VecT " ++ renderChk t1 ++ renderChk t2 ++ renderVal v
-renderInf (VecElimT t1 t2 t3 t4 t5 t6 v) = "VecElimT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4 
-                                                       ++ renderChk t5 ++ renderChk t6 ++ renderVal v
-renderInf (ListT t v) = "ListT " ++ renderChk t ++ renderVal v
-renderInf (ListElimT t1 t2 t3 t4 t5 v) = "ListElimT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4 
-                                                       ++ renderChk t5 ++ renderVal v
-renderInf (FinT t v) = "FinT " ++ renderChk t ++ renderVal v
-renderInf (FinElimT t1 t2 t3 t4 t5 v) = "FinElimT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4 
-                                                    ++ renderChk t5 ++ renderVal v
-renderInf (EqT t1 t2 t3 v) = "EqT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderVal v
-renderInf (EqElimT t1 t2 t3 t4 t5 t6 v) = "EqElimT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4 
-                                                     ++ renderChk t5 ++ renderChk t6 ++ renderVal v
-renderInf (SigElimT t1 t2 t3 t4 t5 v) = "SigElimT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4 
-                                                    ++ renderChk t5 ++ renderVal v
-renderInf (TMaybeT t v) = "TMaybeT " ++ renderChk t ++ renderVal v
-renderInf (LTET t1 t2 v) = "LTET " ++ renderChk t1 ++ renderChk t2 ++ renderVal v
-renderInf (LTEElimT t1 t2 t3 t4 t5 t6 v) = "LTEElimT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4 
-                                                       ++ renderChk t5 ++ renderChk t6 ++ renderVal v
+renderInf (AnnT t1 t2 v) = "(AnnT " ++ renderChk t1 ++ renderChk t2 ++ renderVal v ++")"
+renderInf (StarT v) = "(StarT " ++ renderVal v ++")"
+renderInf (PiT t1 t2 v) = "(PiT " ++ renderChk t1 ++ renderChk t2 ++ renderVal v ++")"
+renderInf (SigmaT t1 t2 v) = "(SigmaT " ++ renderChk t1 ++ renderChk t2 ++ renderVal v ++")"
+renderInf (BoundT i v) = "(BoundT " ++ show i ++ renderVal v  ++")"
+renderInf (FreeT n v) = "(FreeT " ++ "("++show n++")" ++ renderVal v  ++")"
+renderInf (AppRedT ti tc v) = "(" ++ renderInf ti ++ " :@: " ++ renderChk tc ++ renderVal v  ++")"
+renderInf (AppT t1 t2 v) = "(AppT " ++ renderChk t1 ++ renderChk t2 ++ renderVal v ++")"
+renderInf (PairT t1 t2 t3 t4 v) = "(PairT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4 ++ renderVal v ++")"
+renderInf (NatT v) = "(NatT " ++ renderVal v  ++")"
+renderInf (NatElimT t1 t2 t3 t4 v) = "(NatElimT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4 ++ renderVal v ++")"
+renderInf (VecT t1 t2 v) = "(VecT " ++ renderChk t1 ++ renderChk t2 ++ renderVal v ++")"
+renderInf (VecElimT t1 t2 t3 t4 t5 t6 v) = "(VecElimT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4 
+                                                       ++ renderChk t5 ++ renderChk t6 ++ renderVal v ++")"
+renderInf (ListT t v) = "(ListT " ++ renderChk t ++ renderVal v ++")"
+renderInf (ListElimT t1 t2 t3 t4 t5 v) = "(ListElimT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4 
+                                                       ++ renderChk t5 ++ renderVal v ++")"
+renderInf (FinT t v) = "(FinT " ++ renderChk t ++ renderVal v ++")"
+renderInf (FinElimT t1 t2 t3 t4 t5 v) = "(FinElimT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4 
+                                                    ++ renderChk t5 ++ renderVal v ++")"
+renderInf (EqT t1 t2 t3 v) = "(EqT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderVal v ++")"
+renderInf (EqElimT t1 t2 t3 t4 t5 t6 v) = "(EqElimT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4 
+                                                     ++ renderChk t5 ++ renderChk t6 ++ renderVal v ++")"
+renderInf (SigElimT t1 t2 t3 t4 t5 v) = "(SigElimT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4
+                                                    ++ renderChk t5 ++ renderVal v ++")"
+renderInf (TMaybeT t v) = "(TMaybeT " ++ renderChk t ++ renderVal v ++")" 
+renderInf (LTET t1 t2 v) = "(LTET " ++ renderChk t1 ++ renderChk t2 ++ renderVal v ++")"
+renderInf (LTEElimT t1 t2 t3 t4 t5 t6 v) = "(LTEElimT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4 
+                                                       ++ renderChk t5 ++ renderChk t6 ++ renderVal v ++")"
  --     deriving (Show, Eq)
 
 -- Checkable Terms
@@ -114,22 +114,22 @@ data TermChk2 =
  | LTESuccT TermChk2 TermChk2 TermChk2 Value
 
 renderChk :: TermChk2 -> String 
-renderChk (InfT t v) = "InfT " ++ renderInf t ++ renderVal v 
-renderChk (LamT t v) = "LamT " ++ renderChk t ++ renderVal v
-renderChk (ZeroT v) = "ZeroT " ++ renderVal v 
-renderChk (SuccT t v) = "SuccT " ++ renderChk t ++ renderVal v 
-renderChk (NilT t v) = "NilT " ++ renderChk t ++ renderVal v 
-renderChk (ConsT t1 t2 t3 t4 v) = "ConsT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4 ++ renderVal v 
-renderChk (LNilT t v) = "LNilT " ++ renderChk t ++ renderVal v 
-renderChk (LConsT t1 t2 t3 v) = "LConsT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderVal v 
-renderChk (VecToListT t1 t2 t3 v) = "VecToListT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderVal v 
-renderChk (FZeroT t v) = "FZeroT " ++ renderChk t ++ renderVal v 
-renderChk (FSuccT t1 t2 v) = "FSuccT " ++ renderChk t1 ++ renderChk t2 ++ renderVal v 
-renderChk (ReflT t1 t2 v) = "ReflT " ++ renderChk t1 ++ renderChk t2 ++ renderVal v 
-renderChk (TNothingT t v) = "TNothingT " ++ renderChk t ++ renderVal v 
-renderChk (TJustT t1 t2 v) = "TJustT " ++ renderChk t1 ++ renderChk t2 ++ renderVal v 
-renderChk (LTEZeroT t v) = "LTEZeroT " ++ renderChk t ++ renderVal v 
-renderChk (LTESuccT t1 t2 t3 v) = "LTESuccT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderVal v 
+renderChk (InfT t v) = "(InfT " ++ renderInf t ++ renderVal v ++ ")"
+renderChk (LamT t v) = "(LamT " ++ renderChk t ++ renderVal v ++ ")"
+renderChk (ZeroT v) = "(ZeroT " ++ renderVal v  ++ ")"
+renderChk (SuccT t v) = "(SuccT " ++ renderChk t ++ renderVal v  ++ ")"
+renderChk (NilT t v) = "(NilT " ++ renderChk t ++ renderVal v  ++ ")"
+renderChk (ConsT t1 t2 t3 t4 v) = "(ConsT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderChk t4 ++ renderVal v  ++ ")"
+renderChk (LNilT t v) = "(LNilT " ++ renderChk t ++ renderVal v  ++ ")"
+renderChk (LConsT t1 t2 t3 v) = "(LConsT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderVal v  ++ ")"
+renderChk (VecToListT t1 t2 t3 v) = "(VecToListT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderVal v  ++ ")"
+renderChk (FZeroT t v) = "(FZeroT " ++ renderChk t ++ renderVal v  ++ ")"
+renderChk (FSuccT t1 t2 v) = "(FSuccT " ++ renderChk t1 ++ renderChk t2 ++ renderVal v  ++ ")"
+renderChk (ReflT t1 t2 v) = "(ReflT " ++ renderChk t1 ++ renderChk t2 ++ renderVal v  ++ ")"
+renderChk (TNothingT t v) = "(TNothingT " ++ renderChk t ++ renderVal v  ++ ")"
+renderChk (TJustT t1 t2 v) = "(TJustT " ++ renderChk t1 ++ renderChk t2 ++ renderVal v  ++ ")"
+renderChk (LTEZeroT t v) = "(LTEZeroT " ++ renderChk t ++ renderVal v  ++ ")"
+renderChk (LTESuccT t1 t2 t3 v) = "(LTESuccT " ++ renderChk t1 ++ renderChk t2 ++ renderChk t3 ++ renderVal v  ++ ")"
 
   --    deriving (Show, Eq)
 
