999419c488e6d4a43ad21fa696356561c4b417c3
Munsee <msc2@st-andrews.ac.uk>
2024-02-08 14:04:03 +0000

    fix bug on usecase updates

--- src/Refactoring/AddParam.hs
+++ src/Refactoring/AddParam.hs
@@ -46,16 +46,15 @@ instance FromRefactorArgs AddParamArgs where
 -- | Specialise a constructor at a given index to a given term.
 addParam :: AddParamArgs -> Program -> Refact Program
 addParam args (Program items) =
-  return $
-    Program
-      ( map
-          ( \item ->
-              case item of
-                (Decl d) | declName d == addParamFuncName args -> Decl (addParam_func d)
-                _ -> item
-          )
-          items
-      ) --TODO: update usesites of f elsewhere: not implemented
+  let addedParam =   ( map
+                    ( \item ->
+                        case item of
+                          (Decl d) | declName d == addParamFuncName args -> Decl (addParam_func d)
+                          _ -> item
+                    )
+                    items
+                )
+    in return (Program (map updateUseCase addedParam))
   where 
     addParam_func:: DeclItem -> DeclItem 
     addParam_func d = 
@@ -63,42 +62,49 @@ addParam args (Program items) =
           (l,r) = splitAt (addParamIndexPos args) tyList 
           newTyList= l ++ ((Var "newParamV" 0 ,addParamNewTerm args ):r)
           newSig = listToPiType (newTyList, outTy)
-          in d {declTy =newSig, declClauses = map (addParam_cl (declName d)) (declClauses d)}
+          in d {declTy =newSig, declClauses = map addParam_cl  (declClauses d)}
     --deals with equations
-    addParam_cl:: String -> Clause -> Clause
-    addParam_cl funcName (Clause pats term) = 
-      Clause (addParam_eqnlhs pats) (mapTerm  (addParam_eqnrhs funcName) term)
-    addParam_cl funcName (ImpossibleClause pats) = ImpossibleClause (addParam_eqnlhs pats)
+    addParam_cl::  Clause -> Clause
+    addParam_cl (Clause pats term) = 
+      Clause (addParam_eqnlhs pats) term
+    addParam_cl (ImpossibleClause pats) = ImpossibleClause (addParam_eqnlhs pats)
     --add pat var
     addParam_eqnlhs:: [Pat] -> [Pat]
     addParam_eqnlhs pats = 
        let (l,r) = splitAt (addParamIndexPos args) pats 
            in l ++ ((genTerm (V (Var "patV" 0))):r)
     --add holes to recursive calls
-    addParam_eqnrhs:: String -> Term -> MapResult Term
-    addParam_eqnrhs fName (Term (App term1 term2) termDat ) =
+    addParam_eqnrhs:: Term -> MapResult Term
+    addParam_eqnrhs (Term (App term1 term2) termDat ) =
       let (outerTerm, innerTerms) = appToList (Term (App term1 term2) termDat)
         in case termValue (outerTerm) of
             Global str ->
-              if str == fName
+              if str == (addParamFuncName args)
                 then
                   let newInnerTerms = addHolesToPosns ("addParamHole_") innerTerms ([addParamIndexPos args])
                    in Replace (listToApp (outerTerm, newInnerTerms))
                 else Continue
             term -> Continue
-    addParam_eqnrhs fName term = Continue
+    addParam_eqnrhs term = Continue
     --add holes
     addHolesToPosns :: String -> [Term] -> [Int] -> [Term]
     addHolesToPosns holeNamePrefix termList [] = termList
     addHolesToPosns holeNamePrefix termList (i : is) =
-      let (l, r) = splitAt (i+1) termList
+      let (l, r) = splitAt i termList
           stringPrefix = if r == [] then removeSpaces (show (last l)) else removeSpaces (show (head r))
           newVar = Var (holeNamePrefix ++ stringPrefix ++ show i) 0
           addedOne = l ++ (genTerm (Hole newVar)) : r
        in addHolesToPosns holeNamePrefix addedOne [j + 1 | j <- is]
     removeSpaces :: String -> String
     removeSpaces = filter (\c -> (c /= ' '))
-
+    --update usecase of f elsewhere 
+    updateUseCase:: Item -> Item
+    updateUseCase (Decl d) = Decl d {declClauses = map updateUseCase_cl (declClauses d)}
+    updateUseCase (Data d) = Data d
+    updateUseCase_cl:: Clause -> Clause
+    updateUseCase_cl (Clause pats term) = 
+      Clause pats (mapTerm addParam_eqnrhs term)
+    updateUseCase_cl (ImpossibleClause pats) = ImpossibleClause pats
 
 -- stack run -- -r examples/testAddParams.fluid -n add-param -a 'func=f, index=0, type =`Nat`'
 -- stack run -- -r examples/example3.fluid -n add-param -a 'func=lookUpVar, index=0, type =`List Nat`'
